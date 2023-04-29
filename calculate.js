c834ae524f2797fec1787a34f6edc9fe7e16e27d
KristyKostyor <ms.kostyor@gmail.com>
2023-04-29 14:20:18 +0300

    quastion

--- calculate.js
+++ calculate.js
@@ -1,29 +1,27 @@
 'use strict'
+const totalAmountSum = +prompt("Введите сумму");
+const quantyGoodsCart = +prompt("Введите количество товаров");
+const promoCode = prompt("Введите промокод");
 
-
-let totalAmountCart = prompt("Введите сумму");
-let quantyGoodsCart = prompt('Введите количество товаров');
-let promoCode = prompt ('Введите промокод');
-alert(`Ваша итоговая сумма ${totalAmountCart}, ${quantyGoodsCart}, ${promoCode}`);
-
-
-const calculate = (totalAmountCart, quantyGoodsCart, promoCode) => {  
-    let sum = totalAmountCart;
+const calculate = (totalAmountSum, quantyGoodsCart, promoCode) => {  
+    let sum = totalAmountSum;
     let quanty = quantyGoodsCart;
     let promo = promoCode;
 
-    if(quanty>10){
-        sum = sum*0.97;
+    if (quanty > 10) {
+      sum *= 0.97;
     }
-    else if (quanty>3000) {
-        sum = sum - (sum-3000)*0.15;
+    if (sum > 3000) {
+      sum = sum - (sum - 3000) * 0.15;
     }
-    else if (promo === "METHED") {
-        sum = sum*0.9;
+    if (promo === "METHED") {
+        sum*=0.9;
     }
-    else if (promo === "G3H2Z1" && quanty > 2000) {
-        sum-500;
+    else if (promo === "G3H2Z1" && sum > 2000) {
+        sum -= 500;
     }
         return  sum;
     
 };
+
+    alert(calculate(`Ваша итоговая сумма: ${totalAmountSum}, ${quantyGoodsCart}, ${promoCode}`));
